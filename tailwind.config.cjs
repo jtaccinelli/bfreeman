204d8b4056171def35fd284864169097d761c0df
Jordan <jtaccinelli@gmail.com>
2023-06-15 19:25:38 +1000

    feat: migrated all components to be astro friendly

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      aria: {
+        current: 'current="page"',
+      },
+      fontFamily: {
+        sans: "InterVariable, Inter, helvetica, sans-serif",
+      },
+      colors: {
+        gray: require("tailwindcss/colors").neutral,
+        main: require("tailwindcss/colors").emerald,
+      },
+    },
+  },
+  plugins: [],
+  plugins: [],
+};
