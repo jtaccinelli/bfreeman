db68dd3f93405322a6ce0e048f695a79d0f6e8bc
Jordan <jtaccinelli@gmail.com>
2023-06-16 17:50:11 +1000

    feat: finished all new page layouts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,14 @@
 import { z, defineCollection } from "astro:content";
 
-export const samplesSchema = z.object({
-  title: z.string(),
-  where: z.string(),
-  type: z.string(),
-  area: z.string(),
-  purpose: z.string(),
-});
-
 const samplesCollection = defineCollection({
   type: "content",
-  schema: samplesSchema,
+  schema: z.object({
+    title: z.string(),
+    where: z.string(),
+    type: z.string(),
+    area: z.string(),
+    purpose: z.string(),
+  }),
 });
 
 export const collections = {
